eb28c76a80a6eec39ab0518c7707091c842f4aee
Ibrahim Abdulhameed <i.ibrahim.abdulhameed@gmail.com>
2022-12-10 13:45:03 +0100

    feat Create enqueue  and toArray methods

--- data structures/Queue.js
+++ data structures/Queue.js
@@ -19,11 +19,39 @@ class Queue {
     return !this.count;
   }
   //enqueue
+  enqueue(value) {
+    // O(1)
 
+    value = new Node(value);
+    if (this.isEmpty()) {
+      this.head = value;
+    } else {
+      this.tail.next = value;
+      value.previous = this.tail;
+    }
+    this.count++;
+    this.tail = value;
+  }
   // dequeue
   // find
   // toArray
+
+  toArray() {
+    // O(n)
+
+    let values = [];
+    let current = this.head;
+    while (current) {
+      values.push(current.value);
+      current = current.next;
+    }
+    return values;
+  }
 }
 
 const myQueue = new Queue();
-console.log(myQueue.isEmpty());
+myQueue.enqueue(1);
+myQueue.enqueue(2);
+myQueue.enqueue(3);
+myQueue.enqueue(4);
+console.log(myQueue.toArray());
