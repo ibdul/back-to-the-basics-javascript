666fd84c5ccf01332636aa5845f3fa683d473d69
Ibrahim Abdulhameed <i.ibrahim.abdulhameed@gmail.com>
2022-12-10 12:57:43 +0100

    feat Create queue class

---
+++ data structures/Queue.js
@@ -0,0 +1,20 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.previous = undefined;
+    this.next = undefined;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.head = undefined;
+    this.tail = undefined;
+    this.count = 0;
+  }
+  // isEmpty
+  //enqueue
+  // dequeue
+  // find
+  // toArray
+}
