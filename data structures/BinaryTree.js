7a4159684233d86a6a3a46483c0c5aa81497384c
Ibrahim Abdulhameed <i.ibrahim.abdulhameed@gmail.com>
2022-12-10 18:25:12 +0100

    feat Add remove and find methods

--- data structures/BinaryTree.js
+++ data structures/BinaryTree.js
@@ -8,6 +8,7 @@ class Node {
   // add
   add(value) {
     // O(n/2)
+
     value.parent = this;
     if (this.value > value.value) {
       if (this.left) {
@@ -23,13 +24,66 @@ class Node {
       }
     }
   }
+  // remove
+  remove(value, side = undefined) {
+    // O(n/2)
+
+    if (value < this.value) {
+      this.left.remove(value, "left");
+    } else if (value > this.value) {
+      this.right.remove(value, "right");
+    } else if (value == this.value) {
+      if (this.left || this.right) {
+      } else {
+        if (side == "left") {
+          this.parent.left = undefined;
+        } else if (side == "right") {
+          this.parent.right = undefined;
+        }
+      }
+    }
+  }
+  // describe
+  describe(values = []) {
+    // O(n)
+
+    if (this.left) {
+      this.left.describe(values);
+    }
+    values.concat(values.push(this.value));
+    if (this.right) {
+      this.right.describe(values);
+    }
+    return values;
+  }
+
+  find(value) {
+    // O(n/2)
+
+    if (value == this.value) {
+      return this.value;
+    } else if (value < this.value) {
+      return this.left.find(value);
+    } else {
+      return this.right.find(value);
+    }
+  }
 }
 
 class BinaryTree {
   constructor() {
     this.root = undefined;
   }
+  // find
+  find(value) {
+    // O(n/2)
 
+    if (this.root) {
+      return this.root.find(value);
+    }
+    return undefined;
+  }
+  // findAll
   // add
   add(value) {
     // O(n/2)
@@ -42,10 +96,18 @@ class BinaryTree {
       this.root.add(value);
     }
   }
+  // remove
+  remove(value) {
+    // O(n/2)
 
+    if (this.root) {
+      this.root.remove(value);
+    }
+  }
   // describe
   describe() {
-    // O(n);
+    // O(n)
+
     let values = [];
 
     if (!this.root) {
@@ -66,7 +128,7 @@ myTree.add(8);
 myTree.add(4);
 myTree.add(5);
 myTree.add(9);
-// console.log(myTree.find(3));
-// myTree.remove(9);
+console.log(myTree.find(3));
+myTree.remove(9);
 console.log(myTree.describe());
 console.log(myTree.describe());
