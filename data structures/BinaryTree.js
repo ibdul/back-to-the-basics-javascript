a478a926297a910d21c4db564cc560c37728b99c
Ibrahim Abdulhameed <i.ibrahim.abdulhameed@gmail.com>
2022-12-10 16:52:26 +0100

    feat Create BInaryTree class

---
+++ data structures/BinaryTree.js
@@ -0,0 +1,19 @@
+class Node {
+  constructor(value) {
+    this.parent = undefined;
+    this.value = value;
+    this.left = undefined;
+    this.right = undefined;
+  }
+}
+
+class BinaryTree {
+  constructor() {
+    this.root = undefined;
+  }
+  // find
+  // findAll
+  // add
+  // describe
+  // remove
+}
