2116b9815f451128f7dffb03d3db29044b3a2744
Ibrahim Abdulhameed <i.ibrahim.abdulhameed@gmail.com>
2022-12-10 18:22:37 +0100

    feat Create add and describe methods

--- data structures/BinaryTree.js
+++ data structures/BinaryTree.js
@@ -5,15 +5,68 @@ class Node {
     this.left = undefined;
     this.right = undefined;
   }
+  // add
+  add(value) {
+    // O(n/2)
+    value.parent = this;
+    if (this.value > value.value) {
+      if (this.left) {
+        this.left.add(value);
+      } else {
+        this.left = value;
+      }
+    } else {
+      if (this.right) {
+        this.right.add(value);
+      } else {
+        this.right = value;
+      }
+    }
+  }
 }
 
 class BinaryTree {
   constructor() {
     this.root = undefined;
   }
-  // find
-  // findAll
+
   // add
+  add(value) {
+    // O(n/2)
+
+    value = new Node(value);
+
+    if (!this.root) {
+      this.root = value;
+    } else {
+      this.root.add(value);
+    }
+  }
+
   // describe
-  // remove
+  describe() {
+    // O(n);
+    let values = [];
+
+    if (!this.root) {
+      return values;
+    } else {
+      values = this.root.describe();
+    }
+    return values;
+  }
 }
+
+const myTree = new BinaryTree();
+myTree.add(10);
+myTree.add(3);
+myTree.add(1);
+myTree.add(6);
+myTree.add(8);
+myTree.add(4);
+myTree.add(5);
+myTree.add(9);
+// console.log(myTree.find(3));
+// myTree.remove(9);
+console.log(myTree.describe());
+console.log(myTree.describe());
