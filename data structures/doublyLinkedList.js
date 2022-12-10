041ea617c42a39201450a8b4f6673014f50bc01c
Ibrahim Abdulhameed <i.ibrahim.abdulhameed@gmail.com>
2022-12-10 11:52:41 +0100

    init create DoublyLinkedList class

---
+++ data structures/doublyLinkedList.js
@@ -0,0 +1,14 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = undefined;
+    this.previous = undefined;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = undefined;
+    this.tail = undefined;
+  }
+}
